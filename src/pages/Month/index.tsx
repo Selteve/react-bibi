802feb1486ac1a706f00178457f3eb51b95d698f
在我伞下 <uukii5201314@126.com>
2024-09-08 10:51:14 +0800

    更新了月份统计计算

--- src/pages/Month/index.tsx
+++ src/pages/Month/index.tsx
@@ -13,17 +13,34 @@ const Month = () => {
   // 按月分组数据
   const billList: Result[] = useSelector((state: RootState) => state.bill.billList)
   const monthGroup = useMemo(() => { // 按月分组
-    return lodash.groupBy(billList, (item: Result) => dayjs(item.date).format('YYYY - MM'))
+    return lodash.groupBy(billList, (item: Result) => dayjs(item.date).format('YYYY-MM'))
   }, [billList])
-  console.log(monthGroup);
+  // console.log(monthGroup);
   // 控制弹窗的显示状态
   const [show, setShow] = useState(false)
+  // 当前月份支出数据
+  const [ currentMonthBill, setCurrentMonthBill ] = useState<Result[]>([])
+
   // 控制时间的显示状态
-  const [date, setDate] = useState(() => dayjs(new Date()).format('YYYY | MM'))
+  const [date, setDate] = useState(() => dayjs(new Date()).format('YYYY-MM'))
+  // 计算当前月份的账单
+  const monthResult = useMemo(() => {
+    // 支出 、 收入 、 结余
+    const pay = currentMonthBill.filter(item => item.type === 'pay').reduce((pre, cur) => pre + cur.money, 0)
+    const income = currentMonthBill.filter(item => item.type === 'income').reduce((pre, cur) => pre + cur.money, 0)
+    return {
+      pay,
+      income,
+      balance: income + pay
+    }
+
+  }, [currentMonthBill])
   // 选择时间后的回调函数
   const onConfirm = (date: Date) => {
     setShow(false)  // 关闭弹窗
-    const formatDate = dayjs(date).format('YYYY | MM')
+    const formatDate = dayjs(date).format('YYYY-MM')
+    setCurrentMonthBill(monthGroup[formatDate] || [])  // 更新当前月份的账单
+    console.log(monthResult);
     setDate(formatDate)  // 更新时间
   }
   return (
@@ -44,15 +61,15 @@ const Month = () => {
           {/* 统计区域 */}
           <div className='twoLineOverview'>
             <div className="item">
-              <span className="money">2</span>
+              <span className="money">{monthResult.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">23</span>
+              <span className="money">{monthResult.income.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">44</span>
+              <span className="money">{monthResult.balance.toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
